f303b7c11ad0b874b704f6897e5678fe846deb96
7heo <7heo@mail.com>
2014-01-07 07:10:35 +0100

    REFACTOR: Better start/stop script.
    It's in the title

--- start-stop.bat
+++ start-stop.bat
@@ -23,16 +23,15 @@ if exist %SystemRoot%\system32\drivers\etc\hosts.bak (
 	move %SystemRoot%\system32\drivers\etc\hosts.bak %SystemRoot%\system32\drivers\etc\hosts > nul
 	taskkill /F /IM nginx.exe > nul
 	taskkill /F /IM rinetd.exe > nul
-	echo Disabled.
+	msg * /server:127.0.0.1 "Enabled."
+	rem mshta "javascript:alert("Disabled.");close();"
+	rem use that mshta if msg isn't working.
 ) else (
 	copy %SystemRoot%\system32\drivers\etc\hosts %SystemRoot%\system32\drivers\etc\hosts.bak >nul
 	echo 127.0.0.200 store.steampowered.com >> %SystemRoot%\system32\drivers\etc\hosts
 	echo 127.0.0.201 steamcommunity.com >> %SystemRoot%\system32\drivers\etc\hosts
 	start %~dp0\bin\nginx.exe -p %~dp0
 	start /min %~dp0\bin\rinetd.exe -c %~dp0\conf\rinetd.conf
-	echo WARNING: A CMD WINDOW HAS BEEN OPENED MINIMIZED.
-	echo          DO NOT CLOSE IT OR ENHANCED STEAM WON'T WORK.
-	echo Enabled.
+	msg * /server:127.0.0.1 "Enabled."
+	msg * /server:127.0.0.1 "WARNING: A CMD WINDOW HAS BEEN OPENED MINIMIZED. ENHANCED STEAM STANDALONE WON'T WORK IF YOU CLOSE IT."
 )
-
-pause >nul
